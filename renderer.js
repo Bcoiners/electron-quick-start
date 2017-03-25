13008ffb5e3397c260656fc252495d0ef01a1b4e
Raku Watari <rwatari@gmail.com>
2017-03-25 15:14:45 -0700

    Start fullnode on ready

--- renderer.js
+++ renderer.js
@@ -1,3 +1,30 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var bcoin = require('bcoin');
+
+var node = bcoin.fullnode({
+  network: 'simnet',
+  nodes: ['10.7.64.53', 'redsquad.dev.purse.io'],
+  db: 'memory'
+});
+
+async function main() {
+  await node.open();
+
+  await node.connect();
+
+  node.on('connect', function(entry, block) {
+    console.log('%s (%d) added to chain.', entry.rhash(), entry.height);
+    console.log(entry);
+    console.log(block);
+  });
+
+  node.on('tx', function(tx) {
+    console.log('%s added to mempool.', tx.txid());
+  });
+
+  node.startSync();
+}
+
+main();
